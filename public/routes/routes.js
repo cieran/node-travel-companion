af41b3b0567709271c585c23b3de7c9a8979eb9b
Cieran Neeson <cieranneeson@gmail.com>
2017-06-04 16:26:55 +0100

    Have Luas JSON data rendering. Don't know what to do with it though.

--- public/routes/routes.js
+++ public/routes/routes.js
@@ -1,7 +1,9 @@
 'use strict';
 var request = require('request');
-
-
+var eyes = require('eyes');
+var fs = require('fs');
+var parseString = require('xml2js').parseString;
+var http = require('http');
 
 module.exports = function(app){
 
@@ -19,9 +21,8 @@ module.exports = function(app){
 			console.log('Please enter a stop number.');
 		}
 
-		if(service == "Dublin Bus"){
+		if(service == "Dublin Bus" || service == "Bus Eireann"){
 			var url = "https://data.dublinked.ie/cgi-bin/rtpi/realtimebusinformation?stopid=" + stop + "&format=json";
-
 			request.get({
 			    url: url,
 			    json: true,
@@ -36,10 +37,29 @@ module.exports = function(app){
 			      var api_results = data.results;
 			      console.log(api_results);
 			     
-			      	res.render('fetched', {title:'Results', results: api_results});
+			      	res.render('fetched', {title:service + ' Results', results: api_results});
 			    }
 			});
 
+		}else if(service == "Luas"){
+			var url = "http://luasforecasts.rpa.ie/xml/get.ashx?action=forecast&stop=" + stop + "&encrypt=false";
+			request.get({
+				url: url,
+				xml: true,
+				headers: {'User-Agent' : 'request'}
+			}, (err, resp, data) => {
+				if(err){
+					console.log(err);
+				}else if(res.statusCode !== 200){
+					console.log(resp.statusCode);
+				}else{
+					parseString(data, {explicitArray : false}, function (err, result) {
+					    var api_results = JSON.stringify(result);
+					    res.render(service + '-Results', {title:service + ' Results', results: api_results});
+					});
+				}
+			});
+			
 		}else{
 			console.log("service wasn't DB, working on that next.");
 			res.redirect('/');
